1707a6447744e7fb946e9cc244032149035dc782
Sung Ye In <syi9397@naver.com>
2021-05-25 20:27:07 +0900

    UPDATE; main styles

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { SortingWordChange } from "@/libs/style";
 
 const sizes = [
     {
@@ -12,6 +14,28 @@ const sizes = [
     },
 ];
 
+const Sorting = styled.div<any>`
+    align-items: ${(props) => SortingWordChange(props.alignItems)};
+    justify-content: ${(props) => SortingWordChange(props.justifyContent)};
+`;
+
 export const Section = styled.section<{ size: number }>`
+    width: 100%;
     height: ${(props) => sizes[props.size].height};
+    position: relative;
+`;
+
+export const FlexBox = styled(Sorting)`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    ${(props) =>
+        props.direction === "R"
+            ? css`
+                  flex-direction: row;
+              `
+            : props.direction === "C" &&
+              css`
+                  flex-direction: column;
+              `}
 `;
